c9fadf3f95103d96343314126332f49febed4723
nkean <nathankean2@gmail.com>
2018-04-13 16:33:06 -0500

    Adds new employee to table on button click

--- script.js
+++ script.js
@@ -4,4 +4,20 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jQuery has loaded');
+    $('#addEmployeeButton').click(addEmployeeHandler);
+}
+
+function addEmployeeHandler() {
+    console.log('addEmployeeButton clicked');
+
+}
+
+function addNewEmployee() {
+    let firstName = '<td>' + $('#firstNameInput').val() + '</td>';
+    let lastName = '<td>' + $('#lastNameInput').val() + '</td>';
+    let employeeId = '<td>' + $('#employeeIdInput').val() + '</td>';
+    let employeeTitle = '<td>' + $('#employeeTitleInput').val() + '</td>';
+    let employeeSalary = '<td>$ ' + $('#annualSalaryInput').val() + '</td>';
+
+    $('#employeeList').append('<tr>' + firstName + lastName + employeeId + employeeTitle + employeeSalary + '</tr>');
 }
