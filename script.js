0e715467f24fd6f9605d3f5e18307b9e9eb5c2ec
nkean <nathankean2@gmail.com>
2018-04-15 17:49:10 -0500

    Added delete button

--- script.js
+++ script.js
@@ -7,15 +7,16 @@ $(document).ready(onReady);
 function onReady() {
     console.log('jQuery has loaded');
     $('#addEmployeeButton').click(addEmployeeHandler);
+    $('#employeeList').on('click', '.removeEmployeeButton',removeEmployeeHandler);
     updateMonthlyCosts();
-}
+} // end onReady
 
 function addEmployeeHandler() {
     console.log('addEmployeeButton clicked');
     addNewEmployee();
     updateMonthlyCosts();
     clearInputs();
-}
+} // end addEmployeeHander
 
 function addNewEmployee() {
     let firstName = '<td>' + $('#firstNameInput').val() + '</td>';
@@ -23,9 +24,10 @@ function addNewEmployee() {
     let employeeId = '<td>' + $('#employeeIdInput').val() + '</td>';
     let employeeTitle = '<td>' + $('#employeeTitleInput').val() + '</td>';
     let employeeSalary = '<td>$' + Number($('#annualSalaryInput').val()).toLocaleString('en') + '</td>';
+    let removeButton = '<td><button class="removeEmployeeButton">Remove</button>';
 
-    $('#employeeList').append('<tr>' + firstName + lastName + employeeId + employeeTitle + employeeSalary + '</tr>');
-}
+    $('#employeeList').append('<tr>' + firstName + lastName + employeeId + employeeTitle + employeeSalary + removeButton + '</tr>');
+} // end addNewEmployee
 
 function clearInputs() {
     $('#firstNameInput').val('');
@@ -33,7 +35,7 @@ function clearInputs() {
     $('#employeeIdInput').val('');
     $('#employeeTitleInput').val('');
     $('#annualSalaryInput').val('');   
-}
+} // end clearInputs
 
 function updateMonthlyCosts() {
     let newEmployeeSalary = $('#annualSalaryInput').val();
@@ -42,5 +44,11 @@ function updateMonthlyCosts() {
     $('#totalMonthlyCosts').text('Total Monthly: $' + monthlyCostFormatted);
     if (totalMonthlyCosts > 20000) {
         $('#totalMonthlyCosts').css('color', 'red');
-    }
-}
+    } // end if
+} // end updateMonthlyCosts
+
+function removeEmployeeHandler() {
+    console.log('removeEmployeeButton clicked');
+    $(this).parent().parent().remove();
+} // end removeEmployee
+
